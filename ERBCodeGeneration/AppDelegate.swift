55e6da4e69cd5a64a70dfb583823922f75dd5e3f
Ryosuke Ito <rito.0305@gmail.com>
2021-04-07 12:33:46 +0900

    Initial Commit

---
+++ ERBCodeGeneration/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ERBCodeGeneration
+//
+//  Created by Ryosuke Ito on 4/7/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print(Color.red)
+        print(Color.green)
+        print(Color.blue)
+        print(Color.gray)
+        return true
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+ERBCodeGeneration
+=================
+
+An example to generate Swift code with ERB.
+
+
+Pros
+----
+
+- Contrary to other template tools (e.g. Sourcery), ERB is bundled with Xcode by default.
+
+Cons
+----
+
+- Hacky?
